8d4be3972bf069a26ddb649c320c7e7d090bc8f2
Alexa <alexas1903@gmail.com>
2023-02-10 13:06:51 -0300

    version de entrega 10/02/2023

--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
 import validator from './validator.js';
 
-console.log(validator);
+document.getElementById("btn").addEventListener("click", validate);
+document.getElementById("cardNumber").addEventListener("keypress",SoloNumeros);
+//const numero = document.getElementById("cardNumber");
+
+function validate() {
+  const result=document.getElementById("result");
+  const valid=validator.isValid(document.getElementById("cardNumber").value);
+  if (valid) {
+    result.innerHTML = "Su tarjeta es valida " + validator.maskify(document.getElementById("cardNumber").value);    
+  }
+  else {
+    result.innerHTML = "Su tarjeta es invalida " + validator.maskify(document.getElementById("cardNumber").value);  
+  }
+}
+
+function SoloNumeros(evt) {
+
+  const keyNum = (evt.which)? evt.which: evt.keyCode; 
+
+  if (keyNum > 47 && keyNum < 58 || keyNum === 8 || keyNum === 13) {
+    return true;
+  }
+  else {
+    evt.preventDefault();
+    alert("solo números");
+    return false;
+  }
+
+}
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,39 @@
-const validator = {
-  // ...
+const validator =  {
+  isValid,
+  maskify,
+ 
 };
 
+function isValid(numero){
+  if (numero.length === 0) {
+    alert("Debe colocar el numero de su tarjeta");
+    return false;
+  }
+  const begin = numero.split("");
+ 
+  let suma = 0; 
+  for  (let i=0; i<begin.length; i++){  
+    // const posicion = i + 1;
+    if (i % 2 === 0){
+      begin[i] = begin[i] *2;
+
+      if (begin[i] > 9){
+        const num = begin[i].toString().split("");
+        // begin[i] = num[0] + num[1];
+        //console.log(parseInt(num[0]) + parseInt(num[1]), "###");
+        suma += parseInt(num[0]) + parseInt(num[1]);
+      }
+      else {
+        //console.log(begin[i]);
+        suma += begin[i];
+      }
+    }
+  }
+  
+  console.log(suma, "*****");
+  return suma % 10 === 0;
+}
+function maskify(numero){
+  return numero.slice(0, -4).replace(/./g, '#') + numero.slice(-4);
+}
 export default validator;
